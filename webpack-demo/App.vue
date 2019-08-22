12243d12d04379592e5112499bd474f2a6ce8587
gyz147 <18360922022@163.com>
2019-08-22 10:50:03 +0800

    vue单文件组件，结合webpack还有一系列加载器

--- webpack-demo/App.vue
+++ webpack-demo/App.vue
@@ -1,11 +1,31 @@
 <template>
 	<div id="gyz">
-		<h1>welcome to shanghai</h1>
+		<h1>welcome to beijing</h1>
+		<h2 @click="change">{{name}}</h2>
+		<user-list></user-list>
 	</div>
 </template>
 
 <script>
-	console.log(111);
+	//导入模块
+	import User from "./components/User.vue"
+
+
+	export default {
+		data(){
+			return{
+				name:'tom'
+			}
+		},
+		methods:{
+			change(){
+				this.name = '汤姆';
+			}
+		},
+		components:{
+			'user-list':User
+		}
+	}
 </script>
 
 <style>
---
+++ webpack-demo/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="user">
+		<h2>用户列表</h2>
+		<ul>
+			<li v-for="user in users">{{user}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				users:['tom','jack','alice','mark']
+			}
+		},
+		methods:{
+			change(){
+				this.name = '汤姆';
+			}
+		}
+	}
+</script>
+
+<style scoped> /* scoped表示该样式只在当前组件中有效 */
+	.user h2{
+		color: blue;
+	}
+</style>
